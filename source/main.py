1d118a8e77cb796417c83981c1a93b8be1706402
Serphyus <serphyus@gmail.com>
2021-02-25 22:46:35 +0100

    code cleanup

--- source/main.py
+++ source/main.py
@@ -5,7 +5,6 @@ from math import radians, sin, cos
 
 
 
-
 class Cube:
     # check beautifully drawn cube in .png file for visual reference
     verticies = [
@@ -19,7 +18,6 @@ class Cube:
         [-1,  1,  1],   # 8
     ]
 
-    # 
     edges = (
         (0, 1), (0, 3), (0, 4),
         (2, 3), (2, 1), (2, 6),
@@ -27,6 +25,7 @@ class Cube:
         (5, 1), (5, 4), (5, 6)
     )
 
+    orientation = [0, 0, 0]
     point_colors = [[randint(0, 255) for i in range(3)] for i in range(len(verticies))]
     line_colors = [[randint(0, 255) for i in range(3)] for i in range(len(edges))]
 
@@ -39,9 +38,49 @@ class Cube:
         cls.verticies = points
 
 
+    @classmethod
+    def rotateCube(cls, rotation: tuple) -> None:
+        """
+        :param rotation: roll, yaw, pitch rotation values (α, β, γ)
+
+        α: roll  (z rotation)
+        β: yaw   (y rotation)
+        γ: pitch (x rotation)
+
+                            [ cosα -sinα  0 ] [ cosβ   0   sinβ ] [ 1   0     0   ]
+        Rz(α) Ry(β) Rx(γ) = [ sinα  cosα  0 ] [ 0      1    0   ] [ 0  cosγ -sinγ ]
+                            [  0     0    1 ] [ -sinβ  0   cosβ ] [ 0  sinγ  cosγ ]
+
+            [cosα cosβ    cosα sinβ sinγ - sinα cosγ    cosα sinβ cosγ + sinα sinγ]
+        R = [sinα cosβ    sinα sinβ sinγ + cosα cosγ    sinα sinβ cosγ - cosα sinγ]
+            [ -sinβ              cosβ sinγ                      cosβ cosγ         ]
+
+        """
+        γ = radians(rotation[0])
+        β = radians(rotation[1])
+        α = radians(rotation[2])
+        
+        matrix = [
+            [cos(α) * cos(β),    cos(α) * sin(β) * sin(γ) - sin(α) * cos(γ),    cos(α) * sin(β) * cos(γ) + sin(α) * sin(γ)],
+            [sin(α) * cos(β),    sin(α) * sin(β) * sin(γ) + cos(α) * cos(γ),    sin(α) * sin(β) * cos(γ) - cos(α) * sin(γ)],
+            [    -sin(β),                   cos(β) * sin(γ),                                   cos(β) * cos(γ)            ]
+        ]
+
+        new_verticies = []
+        for (x, y, z) in cls.verticies:
+            new_point = []
+            for vector in matrix:
+                new_point.append(
+                    ((x * vector[0]) + (y * vector[1]) + (z * vector[2]))
+                )
+            new_verticies.append(new_point)
+        
+        cls.verticies = new_verticies
+
+
 
 class Gui:
-    def __init__(self, resolution: tuple, fps: int):
+    def __init__(self, resolution: tuple, fps: int, display_verticies: bool, display_edges: bool):
         pygame.init()
 
         self.display = pygame.display.set_mode(resolution)
@@ -55,6 +94,9 @@ class Gui:
         self.clock = pygame.time.Clock()
         self.fps = fps
 
+        self.display_verticies = display_verticies
+        self.display_edges = display_edges
+
         self.font = pygame.font.Font(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'coolvetica.ttf'), 25)
     
 
@@ -63,15 +105,17 @@ class Gui:
         # to draw on a 2 dimentional surface since the y axis is not physically
         # possible to draw in a 3 dimentional space on a 2 dimentional surface
 
-        for index, cordinates in enumerate(Cube.verticies):
-            center = tuple([(self.center[i] + cordinates[i] * self.upscale) for i in (0, 2)])
-            pygame.draw.circle(self.display, Cube.line_colors[index], center, 5)
+        if self.display_verticies:
+            for index, cordinates in enumerate(Cube.verticies):
+                center = tuple([(self.center[i] + cordinates[i] * self.upscale) for i in (0, 2)])
+                pygame.draw.circle(self.display, Cube.point_colors[index], center, 5)
         
-        for index, edges in enumerate(Cube.edges):
-            points = []
-            for i in edges:
-                points.append(tuple([self.center[c] + Cube.verticies[i][c] * self.upscale for c in (0, 2)]))
-            pygame.draw.line(self.display, (Cube.line_colors[index]), points[0], points[1], 2)
+        if self.display_edges:
+            for index, edges in enumerate(Cube.edges):
+                points = []
+                for i in edges:
+                    points.append(tuple([self.center[c] + Cube.verticies[i][c] * self.upscale for c in (0, 2)]))
+                pygame.draw.line(self.display, (Cube.line_colors[index]), points[0], points[1], 2)
 
     
     def drawText(self, text: str, position: tuple) -> None:
@@ -85,11 +129,13 @@ class Gui:
 
 
     def displayInfo(self) -> None:
+        # FPS
         self.drawText(
             text=f'FPS - {self.clock.get_fps():.2f}',
             position=(8, 5)
         )
         
+        # Controls
         self.drawText('X rotation - U/J', (8, 30),)
         self.drawText('Y rotation - I/K', (8, 50),)
         self.drawText('Z rotation - O/L', (8, 70),)
@@ -104,12 +150,12 @@ class Gui:
 
             pressed_keys = pygame.key.get_pressed()
 
-            if pressed_keys[pygame.K_u]:   Cube.updateVerticies(generalRotation((rotation_speed, 0, 0)))
-            elif pressed_keys[pygame.K_i]: Cube.updateVerticies(generalRotation((0, rotation_speed, 0)))
-            elif pressed_keys[pygame.K_o]: Cube.updateVerticies(generalRotation((0, 0, rotation_speed)))
-            elif pressed_keys[pygame.K_j]: Cube.updateVerticies(generalRotation((-rotation_speed, 0, 0)))
-            elif pressed_keys[pygame.K_k]: Cube.updateVerticies(generalRotation((0, -rotation_speed, 0)))
-            elif pressed_keys[pygame.K_l]: Cube.updateVerticies(generalRotation((0, 0, -rotation_speed)))
+            if pressed_keys[pygame.K_u]:   Cube.rotateCube((rotation_speed, 0, 0))
+            elif pressed_keys[pygame.K_i]: Cube.rotateCube((0, rotation_speed, 0))
+            elif pressed_keys[pygame.K_o]: Cube.rotateCube((0, 0, rotation_speed))
+            elif pressed_keys[pygame.K_j]: Cube.rotateCube((-rotation_speed, 0, 0))
+            elif pressed_keys[pygame.K_k]: Cube.rotateCube((0, -rotation_speed, 0))
+            elif pressed_keys[pygame.K_l]: Cube.rotateCube((0, 0, -rotation_speed))
 
             self.display.fill((0, 0, 0))
             self.displayInfo()
@@ -120,47 +166,10 @@ class Gui:
 
 
 
-def generalRotation(rotation: tuple) -> list:
-    """
-     :param rotation: roll, yaw, pitch rotation values (α, β, γ)
-
-     α: roll  (z rotation)
-     β: yaw   (y rotation)
-     γ: pitch (x rotation)
-
-                         [ cosα -sinα  0 ] [ cosβ   0   sinβ ] [ 1   0     0   ]
-     Rz(α) Ry(β) Rx(γ) = [ sinα  cosα  0 ] [ 0      1    0   ] [ 0  cosγ -sinγ ]
-                         [  0     0    1 ] [ -sinβ  0   cosβ ] [ 0  sinγ  cosγ ]
-
-         [cosα cosβ    cosα sinβ sinγ - sinα cosγ    cosα sinβ cosγ + sinα sinγ]
-     R = [sinα cosβ    sinα sinβ sinγ + cosα cosγ    sinα sinβ cosγ - cosα sinγ]
-         [ -sinβ              cosβ sinγ                      cosβ cosγ         ]
-
-    """
-    γ = radians(rotation[0])
-    β = radians(rotation[1])
-    α = radians(rotation[2])
-    
-    matrix = [
-        [cos(α) * cos(β),    cos(α) * sin(β) * sin(γ) - sin(α) * cos(γ),    cos(α) * sin(β) * cos(γ) + sin(α) * sin(γ)],
-        [sin(α) * cos(β),    sin(α) * sin(β) * sin(γ) + cos(α) * cos(γ),    sin(α) * sin(β) * cos(γ) - cos(α) * sin(γ)],
-        [    -sin(β),                   cos(β) * sin(γ),                                   cos(β) * cos(γ)            ]
-    ]
-
-    output = []
-    for (x, y, z) in Cube.verticies:
-        new_point = []
-        for vector in matrix:
-            new_point.append(
-                ((x * vector[0]) + (y * vector[1]) + (z * vector[2]))
-            )
-        output.append(new_point)
-    
-    return output
-
-
-
-
 if __name__ == '__main__':
-    gui = Gui(resolution=(800, 800), fps=60)
+    gui = Gui(
+        resolution=(800, 800), fps=60,
+        display_edges=True, display_verticies=True
+    )
+    
     gui.mainLoop(rotation_speed=1)
